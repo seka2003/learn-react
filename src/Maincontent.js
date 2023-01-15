76f24774c5df22d4b1bf93e082f9b55b720e4e88
KGs_NumS <zamnbbnmza33@gmail.com>
2023-01-15 16:41:25 +0700

    Adding main and subcontent from tomorrow script

---
+++ src/Maincontent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main>
+      <div className="container py-4">
+        <header className="pb-3 mb-4 border-bottom">
+          <a href="/" className="d-flex align-items-center text-dark text-decoration-none">
+            <img src="https://i.imgflip.com/77gjfs.jpg" width="100" height="85" className="me-2" alt="Bootstrap" />
+            <span className="fs-4 fw-bold">Prayut Chan-o-cha</span>
+          </a>
+        </header>
+      </div>
+    </main>
+  );
+}
+
+export default MainContent;
